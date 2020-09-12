f2efc2046879c93ba302cc5537306eef74b17eb7
Joshua Yoes <joshua.r.yoes@gmail.com>
2020-09-12 16:37:42 -0700

    Add material-ui theme to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,28 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import "typeface-montserrat";
+import "typeface-merriweather";
 
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism.css";
+
+import React from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#162918",
+    },
+    secondary: {
+      main: "#297b2e",
+    },
+  },
+});
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {element}
+    </ThemeProvider>
+  );
+};
